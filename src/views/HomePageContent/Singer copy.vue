1ad392231fb72a24ce22fa00390cc1c10ea4b9b1
qyh <1050258124@qq.con>
2020-07-09 22:21:47 +0800

    全部完成

---
+++ src/views/HomePageContent/Singer copy.vue
@@ -0,0 +1,49 @@
+<template>
+    <div>
+        歌手
+        {{qyh.name}}
+        <button @click="logut">退出登录</button>
+
+
+        <ul>
+            <li v-for="item in da" :key="item">{{item}}</li>
+        </ul>
+    </div>
+</template>
+
+
+
+<script>
+    import {mapGetters, mapState} from 'vuex'
+export default {
+    methods:{
+        logut(){
+            // this.$store.commit('ceshi',{
+            //     qyh:{
+            //         name:'sss'
+            //     }
+            // })
+
+            this.$store.commit({
+                type:'ceshi',
+                qyh:{
+                    name:'修改后的名字'
+                }
+
+            })
+        }
+
+    },
+    computed:{
+        /*return qyh(){
+            this.$store.state.qyh.name
+        }
+        */
+        ...mapGetters(["da"]),
+        ...mapState(["qyh"])
+    }
+}
+</script>
+
+
+
