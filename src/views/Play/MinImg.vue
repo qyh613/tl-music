c2c17c93a7e67025a05ef5c179b1d1cd4530916c
qyh <1050258124@qq.con>
2020-07-10 11:22:58 +0800

    修改音乐播放旋转暂停bug

--- src/views/Play/MinImg.vue
+++ src/views/Play/MinImg.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="MinImgBox">
-        <img :src="ImgSrc" alt="" :class="rotating?'xuanzhuan':''">
+        <img :src="ImgSrc" alt="" class="xuanzhuan"
+             :style="rotating?'animation-play-state: running':'animation-play-state: paused'">
     </div>
 </template>
 
@@ -8,9 +9,9 @@
     // 播放页中间图片*************************************
     export default {
         name: "MinImg",
-        props:{
-            ImgSrc:[String],
-            rotating:[Boolean],
+        props: {
+            ImgSrc: [String],
+            rotating: [Boolean],
         }
     }
 </script>
@@ -20,21 +21,23 @@
         text-align: center;
         margin-top: 40px;
         height: 200px;
-        img{
+
+        img {
             height: 100%;
             border-radius: 50%;
 
 
         }
+
         .xuanzhuan {
             animation: animal 8s linear infinite;
         }
 
         @keyframes animal {
-            0%{
+            0% {
                 transform: rotate(0deg);
             }
-            100%{
+            100% {
                 transform: rotate(360deg);
             }
         }
